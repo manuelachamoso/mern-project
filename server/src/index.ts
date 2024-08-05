e841c1937d38f0fc0a25d01c8e4700377985f847
manuelachamoso <manuela.chamoso@gmail.com>
2024-08-06 00:01:44 +0200

    set up mongodb atlas and mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response } from 'express';
+import mongoose from'mongoose';
+import Deck from './models/Deck';
 
+const PORT = 5000;
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+app.post('/decks', async (req: Request, res: Response) => {
+    const newDeck = new Deck({ 
+        title: 'New Deck' 
+    });
+    const createdDeck = await newDeck.save();
+    res.json(createdDeck);
 })
 
-app.get('/another', (req: Request, res: Response) => {
-    res.send('Hello, from another page!');
+mongoose.connect('mongodb+srv://manuelachamoso:dPTIQtkR1rCHbrat@cluster0.ixtkmt4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT);
 })
 
-app.listen(5000);
+
